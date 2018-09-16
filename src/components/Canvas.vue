ada6fed7283ffdf4b775b354fc780a741f470054
Gerd Hannebacher <13304063+gerdfather@users.noreply.github.com>
2018-09-16 16:22:09 +0200

    Added canvas component. Canvas property width will be updated on window resize.

---
+++ src/components/Canvas.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="canvas" :style="{ width: width, height: height }" ref="canvas">
+      Canvas<br>
+      width: {{width}}<br>
+      height: {{height}}<br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Canvas',
+  computed: {
+    dimensions () {
+      return this.$store.getters.getDimensions
+    },
+    width () {
+      return this.dimensions.width
+    },
+    height () {
+      return this.dimensions.height
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .canvas { background-color: coral; }
+</style>
--- src/main.js
+++ src/main.js
@@ -6,5 +6,22 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted () {
+    this.$nextTick(function () {
+      this.$store.dispatch('setWidth', this.getWidth())
+      window.addEventListener('resize', this.setDimensions)
+    })
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.setDimensions)
+  },
+  methods: {
+    setDimensions (event) {
+      this.$store.dispatch('setWidth', this.getWidth())
+    },
+    getWidth () {
+      return this.$el.offsetWidth + 'px'
+    }
+  }
 }).$mount('#app')
